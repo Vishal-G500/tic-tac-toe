d81b4c747342616a960985a76eef324f79ed93d3
Vishal Gupta <vishalgupta@Vishals-MacBook-Air.local>
2024-09-29 17:58:16 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tic-tac-toe/',
   plugins: [react()],
   build: {
     outDir: 'build', // This is the directory where the build files will go
